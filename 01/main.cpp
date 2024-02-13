e867e40545efbd15df2d8b3b11ff3a75c7e11bc2
Lana <115147586+ricorett@users.noreply.github.com>
2024-02-13 11:56:26 +0300

    fix 01 task

--- 01/main.cpp
+++ 01/main.cpp
@@ -9,13 +9,15 @@ int main(){
                 std::cout << "Работаю в режиме тренировки. " << std::endl;
 #endif
 #if MODE == 1
-                std::cout << "Работаю в боевом режиме:"  << std::endl << "Введите первое число: ";
+                std::cout << "Работаю в боевом тренировки: "  << std::endl << "Введите первое число: ";
                 std::cin >> num1;
                 std::cout << "Введите второе число: ";
                 std::cin >> num2;
                 std::cout << "Результат сложения: "  << add(num1, num2) << std::endl;    
+#elif MODE != 1 || MODE != 0
+        std::cout "Неизвестный режим. Завершение работы"
 #else
-#error "Неизвестный режим. Завершение работы"
+#error "MODE не определен."
 #endif
 
     return 0;
